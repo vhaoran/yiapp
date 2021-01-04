40b18ed722a02c0e725233c0dcd59ad2b1cf1f80
whr <whr@whr.local>
2021-01-04 15:53:55 +0800

    aaaa

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:provider/provider.dart';
 import 'package:yiapp/ui/provider/cus_provider.dart';
 import 'ui/hongyun_app.dart';
 
-// ------------------------------------------------------
+//------------------------------------------------------
 // author：suxing
 // date  ：2020/12/8 上午10:40
 // usage ：鸿运来程序入口
-// ------------------------------------------------------
+//------------------------------------------------------
 
 void main() {
   runApp(
